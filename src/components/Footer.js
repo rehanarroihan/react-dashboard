fb7ef6d6af303c9c25003f4aac962f6d0a029c95
rehanarroihan <third2014project@gmail.com>
2020-08-26 21:17:43 +0700

    init react router and menu list

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 class Footer extends Component {
   state = {  }
   render() { 
     return ( 
-      <footer className="main-footer">
-        <div className="footer-left">
-          Copyright © 2018 <div className="bullet" /> Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
-        </div>
-        <div className="footer-right">
-          2.3.0
-        </div>
-      </footer>
+      <Router>
+        <footer className="main-footer">
+          <div className="footer-left">
+            Copyright © 2018 <div className="bullet" /> Design By 
+            <Link to="/about" />Muhamad Nauval Azhar
+          </div>
+          <div className="footer-right">
+            2.3.0
+          </div>
+        </footer>
+      </Router>
     );
   }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link, BrowserRouter as Router } from "react-router-dom";
 import avatar from './../avatar/avatar-1.png';
 
 class Header extends Component {
@@ -6,42 +7,58 @@ class Header extends Component {
 
   render() { 
     return ( 
-      <nav className="navbar navbar-expand-lg main-navbar">
-        <form className="form-inline mr-auto">
-          <ul className="navbar-nav mr-3">
-            <li><a href="#" data-toggle="sidebar" className="nav-link nav-link-lg"><i className="fas fa-bars" /></a></li>
-            <li><a href="#" data-toggle="search" className="nav-link nav-link-lg d-sm-none"><i className="fas fa-search" /></a></li>
-          </ul>
-          <div className="search-element">
-            <input className="form-control" type="search" placeholder="Search" aria-label="Search" data-width={250} />
-            <button className="btn" type="submit"><i className="fas fa-search" /></button>
-            <div className="search-backdrop" />
-            
-          </div>
-        </form>
-        <ul className="navbar-nav navbar-right">
-          <li className="dropdown"><a href="#" data-toggle="dropdown" className="nav-link dropdown-toggle nav-link-lg nav-link-user">
-              <img alt="image" srcSet={ avatar } className="rounded-circle mr-1" />
-              <div className="d-sm-none d-lg-inline-block">Hi, Ujang Maman</div></a>
-            <div className="dropdown-menu dropdown-menu-right">
-              <div className="dropdown-title">Logged in 5 min ago</div>
-              <a href="features-profile.html" className="dropdown-item has-icon">
-                <i className="far fa-user" /> Profile
-              </a>
-              <a href="features-activities.html" className="dropdown-item has-icon">
-                <i className="fas fa-bolt" /> Activities
-              </a>
-              <a href="features-settings.html" className="dropdown-item has-icon">
-                <i className="fas fa-cog" /> Settings
-              </a>
-              <div className="dropdown-divider" />
-              <a href="#" className="dropdown-item has-icon text-danger">
-                <i className="fas fa-sign-out-alt" /> Logout
-              </a>
+      <Router>
+        <nav className="navbar navbar-expand-lg main-navbar">
+          <form className="form-inline mr-auto">
+            <ul className="navbar-nav mr-3">
+              <li>
+                <Link
+                  to={""}
+                  data-toggle="sidebar"
+                  className="nav-link nav-link-lg">
+                  <i className="fas fa-bars" /></Link>
+              </li>
+              <li>
+                <Link
+                  to={""}
+                  data-toggle="search"
+                  className="nav-link nav-link-lg d-sm-none">
+                  <i className="fas fa-search" />
+                  </Link>
+                </li>
+            </ul>
+            <div className="search-element">
+              <input className="form-control" type="search" placeholder="Search" aria-label="Search" data-width={250} />
+              <button className="btn" type="submit"><i className="fas fa-search" /></button>
+              <div className="search-backdrop" />
             </div>
-          </li>
-        </ul>
-      </nav>
+          </form>
+          <ul className="navbar-nav navbar-right">
+            <li className="dropdown">
+              <Link to={""} data-toggle="dropdown" className="nav-link dropdown-toggle nav-link-lg nav-link-user">
+                <img alt="Avatar of User" src={ avatar } className="rounded-circle mr-1" />
+                <div className="d-sm-none d-lg-inline-block">Hi, Ujang Maman</div>
+              </Link>
+              <div className="dropdown-menu dropdown-menu-right">
+                <div className="dropdown-title">Logged in 5 min ago</div>
+                <Link to={""} className="dropdown-item has-icon">
+                  <i className="far fa-user" /> Profile
+                </Link>
+                <Link to={""} className="dropdown-item has-icon">
+                  <i className="fas fa-bolt" /> Activities
+                </Link>
+                <Link to={""} className="dropdown-item has-icon">
+                  <i className="fas fa-cog" /> Settings
+                </Link>
+                <div className="dropdown-divider" />
+                <Link to={ "" } className="dropdown-item has-icon text-danger">
+                  <i className="fas fa-sign-out-alt" /> Logout
+                </Link>
+              </div>
+            </li>
+          </ul>
+        </nav>
+      </Router>
     );
   }
 }
